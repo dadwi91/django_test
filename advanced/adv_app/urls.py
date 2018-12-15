213ae43f86b6259b8240bf3e7c3f01e28a1f7a52
Dq <adhitya.dicky@gmail.com>
2018-12-15 17:53:54 +0900

    reinitialized

---
+++ advanced/adv_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from adv_app import views
+
+#template tagging
+app_name = 'adv_app'
+
+urlpatterns = [
+    path('',views.SchoolListView.as_view(),name='list'),
+    path('<int:pk>/',views.SchoolDetailView.as_view(),name='detail'),
+    path('create/',views.SchoolCreateView.as_view(),name='create'),
+    path('update/<int:pk>/',views.SchoolUpdateView.as_view(),name='update'),
+    path('delete/<int:pk>/',views.SchoolDeleteView.as_view(),name='delete'),
+]
